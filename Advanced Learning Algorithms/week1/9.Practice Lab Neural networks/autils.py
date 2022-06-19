0857d4d13271190976ef6b10573a35f3b02ecab7
kaieye <74253784+kaieye@users.noreply.github.com>
2022-06-19 17:24:00 +0800

    Add files via upload

---
+++ Advanced Learning Algorithms/week1/9.Practice Lab Neural networks/autils.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def load_data():
+    X = np.load("data/X.npy")
+    y = np.load("data/y.npy")
+    X = X[0:1000]
+    y = y[0:1000]
+    return X, y
+
+def load_weights():
+    w1 = np.load("data/w1.npy")
+    b1 = np.load("data/b1.npy")
+    w2 = np.load("data/w2.npy")
+    b2 = np.load("data/b2.npy")
+    return w1, b1, w2, b2
+
+def sigmoid(x):
+    return 1. / (1. + np.exp(-x))
